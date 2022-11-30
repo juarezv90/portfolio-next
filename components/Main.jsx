a36a287d1f6e8a418560a0ce6c25ccfedc4732ac
Victor Juarez <juarezv1990@gmail.com>
2022-11-30 13:08:20 -0500

    updated main

--- components/Main.jsx
+++ components/Main.jsx
@@ -9,17 +9,14 @@ const Main = () => {
       <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
         <div>
           <p className="uppercase text-sm tracking-widest text-gray-600">
-            Let's build something together
+            Let's build something  Different
           </p>
           <h1 className="py-4 text-gray-700">
             Hi, I'm <span className="text-[#5651e5]">Victor</span>
           </h1>
           <h1 className="py-4 text-gray-700"> A Front-End Web Developer</h1>
           <p className="py-4 text-gray-600 max-w-[70%] m-auto">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis,
-            nemo labore necessitatibus illo voluptatum cum consequuntur
-            blanditiis, officia recusandae tempora vel quibusdam commodi
-            cupiditate voluptas aperiam maxime eaque repellat et.
+            I am a frond-end freelance web developer looking to bring your next idea to life. Specializing in online single page applications and mobile friendly web environments.
           </p>
 
           <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
